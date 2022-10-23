274fc2583a3fc135108347059caa5f66a8b65c7a
muratbek2255 <muratbek2255@gmail.com>
2022-10-23 17:38:12 +0600

    create starting app

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.9
+
+WORKDIR /app
+
+COPY ./requirements.txt /app/requirements.txt
+RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
+COPY . /app/
+EXPOSE 8000
+CMD ["uvicorn", "main:app", "--host=0.0.0.0" , "--reload" , "--port", "8000"]
---
+++ core/settings.py
@@ -0,0 +1,22 @@
+import os
+
+PROJECT_NAME = "App"
+SERVER_HOST = 'http://127.0.0.1:8080'
+
+# Secret key
+SECRET_KEY = b"awubsyb872378t^*TG8y68&*&&*8y8yg9POB)*896ft7CR^56dfYUv"
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+API_V1_STR = "/api/v1"
+
+# Token 60 minutes * 24 hours * 8 days = 8 days
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7
+
+# CORS
+BACKEND_CORS_ORIGINS = [
+    "http://localhost",
+    "http://localhost:4200",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
